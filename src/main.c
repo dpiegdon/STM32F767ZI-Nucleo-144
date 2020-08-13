be09fde525defa26da82bb0e3987bb631e991b2a
David R. Piegdon <dgit@piegdon.de>
2020-08-13 19:50:11 +0000

    equalize example code

--- src/main.c
+++ src/main.c
@@ -3,15 +3,23 @@
 
 int main(void)
 {
-	BSP_LED_Init(LED_GREEN);
+	BSP_LED_Init(LED1);
+	BSP_LED_Init(LED2);
+	BSP_LED_Init(LED3);
 
 	for(;;)
 	{
-		BSP_LED_Toggle(LED_GREEN);
-
-		for (int j = 0; j < 1000000; j++)
+		BSP_LED_Toggle(LED1);
+		for (int j = 0; j < 100000; j++)
 			;
 
+		BSP_LED_Toggle(LED2);
+		for (int j = 0; j < 100000; j++)
+			;
+
+		BSP_LED_Toggle(LED3);
+		for (int j = 0; j < 100000; j++)
+			;
 	}
 
 	return 0;
